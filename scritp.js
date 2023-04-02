1f126c20b3905f2929da73e0418896c049b4ead9
alejandra <alejandra.humanes93@gmail.com>
2023-04-02 20:24:37 +0200

    nuevo cambio

--- scritp.js
+++ scritp.js
@@ -5,3 +5,20 @@ window.onload= function(){
     })
 
 }
+/*const add = (a = 1, b = 1 ) =>
+ console.log ("add");*/
+
+ let map = L.map('map').setView([40.416775, -3.703790], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([40.416775,-3.703790 ]).addTo(map).bindPopup("mi nueva ubicacion").openPopup()   /* .bindPopup('calle Calle del Marqués de Valdeiglesias,<br> -izq 28004 Madrid.<br> ')
+    .openPopup( ) */
+   
+
+   
+   
+
+    
